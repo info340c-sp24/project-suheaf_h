f75562deac5df1fc099144ff23d418bdd0275cb0
Suheaf Hussein <suheaf@uw.edu>
2024-05-01 14:48:23 -0700

    Changes made to app

--- dream-team-react/src/App.js
+++ dream-team-react/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Interest from './Interest.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavScrollExample from './NavBar';
+import Feed from './Feed.js'
 function App() {
   return (
     <div>
     <NavScrollExample />
-    <Interest />
+  {/* * <Interest /> */}
+    <Feed />
     </div>
   );
 }
